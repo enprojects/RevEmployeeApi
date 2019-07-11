665253a7bf394cb5f4682ec44d6d2a110e8eb93a
meproject <eyaln79@gmail.com>
2019-07-11 18:52:31 +0300

    save changes

--- Startup.cs
+++ Startup.cs
@@ -17,7 +17,7 @@ namespace EmployeeApi
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration; //
+            Configuration = configuration; // 
         }
 
         public IConfiguration Configuration { get; }
